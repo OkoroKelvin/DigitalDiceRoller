2a7d180515505b437b7890f1d2b6f77887c2bf31
OkoroKelvin <okorokelvinemoakpooke@yahoo.com>
2021-11-07 18:44:17 +0100

    Dice result can be summed and displayed

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -19,8 +19,18 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button2)
         rollButton.setOnClickListener {
-            rollDice1()
-            rollDice2()
+            val cDice1 = rollDice1()
+            val cDice2 = rollDice2()
+            val sumDice = cDice1 + cDice2
+
+            val resultTextView: TextView = findViewById(R.id.textView2)
+            val resultTextView2: TextView = findViewById(R.id.textView3)
+            val resultTextView3: TextView = findViewById(R.id.textView)
+
+            resultTextView.text = cDice1.toString()
+            resultTextView2.text = cDice2.toString()
+            resultTextView3.text = sumDice.toString()
+
         }
 
 
@@ -30,24 +40,18 @@ class MainActivity : AppCompatActivity() {
     /**
      * Roll the dice and update the screen with the result.
      */
-    private fun rollDice1() {
+    private fun rollDice1(): Int {
         // Create new Dice object with 6 sides and roll it
         val dice1 = Dice(6)
-        val diceRoll = dice1.roll()
+        return dice1.roll()
 
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView2)
-        resultTextView.text = diceRoll.toString()
     }
 
-    private fun rollDice2() {
+    private fun rollDice2(): Int {
         // Create new Dice object with 6 sides and roll it
         val dice2 = Dice(6)
-        val diceRoll = dice2.roll()
+        return dice2.roll()
 
-        // Update the screen with the dice roll
-        val resultTextView2: TextView = findViewById(R.id.textView3)
-        resultTextView2.text = diceRoll.toString()
     }
 }
 
